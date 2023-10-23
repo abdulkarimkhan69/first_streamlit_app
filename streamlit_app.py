5af329bc2629d43110da8068fb7da7a198a3c61f
abdulkarimkhan69 <148750208+abdulkarimkhan69@users.noreply.github.com>
2023-10-23 11:06:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
+import pandas
 streamlit.title('My parents healthy diner')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
