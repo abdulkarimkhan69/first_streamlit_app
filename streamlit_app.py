8b9c5348e0a53a033bdc6feebfab83d05f4d1510
abdulkarimkhan69 <148750208+abdulkarimkhan69@users.noreply.github.com>
2023-10-24 10:20:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ streamlit.title('My parents healthy diner')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+streamlit.body('This is body of page')
